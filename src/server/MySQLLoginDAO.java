bd96eed41321d2e460239e59c98fd62e597af932
Martin Vieth <martin_123_vieth@hotmail.com>
2015-08-07 16:20:57 +0200

    added code for database functions with user

--- src/server/MySQLLoginDAO.java
+++ src/server/MySQLLoginDAO.java
@@ -3,7 +3,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.ArrayList;
-
 import shared.DALException;
 import shared.UserDTO;
 
@@ -23,28 +22,31 @@ public class MySQLLoginDAO {
 		}
 	}
 
-	public UserDTO getUser() throws DALException {
+	public UserDTO getUser(String email) throws DALException {
 		ResultSet rs = Connector.doQuery("");
 		try {
-			if (!rs.first())
-				throw new DALException("Brugeren findes ikke");
-			return new UserDTO();
+			if (!rs.first()) throw new DALException("Brugeren "+email+" findes ikke");
+			return new UserDTO(rs.getString("email"),rs.getString("passwd"),rs.getInt("activated"));
 		} catch (SQLException e) {
 			throw new DALException(e.getMessage());
 		}
 	}
 
 	public void createUser(UserDTO user) throws DALException {
-		Connector.doUpdate("");
+		Connector.doUpdate("INSERT INTO user(email, passwd, activated) VALUES " +
+						"(" + user.getEmail() + ", '" + user.getPasswd() + "', '" + user.getActivated() );
 	}
 
 	public void updateUser(UserDTO user) throws DALException {
-
+		Connector.doUpdate(
+				"UPDATE user SET  email = '" + user.getEmail() + "', passwd =  '" + user.getPasswd() + 
+				"', activated = '" + user.getActivated()
+				);
 	}
 
 	public List<UserDTO> getUserList() throws DALException {
 		List<UserDTO> list = new ArrayList<UserDTO>();
-		ResultSet rs = Connector.doQuery("SELECT * FROM operatoer");
+		ResultSet rs = Connector.doQuery("SELECT * FROM user");
 		try {
 			while (rs.next()) {
 				list.add(new UserDTO());
@@ -56,7 +58,7 @@ public class MySQLLoginDAO {
 	}
 
 	public void deleteUser(UserDTO user) throws DALException{
-		Connector.doUpdate("");
+		Connector.doUpdate("DELETE FROM user WHERE email ="+ user.getEmail());
 	}
 	public UserDTO loginCheck() throws DALException{
 		UserDTO user = null;
--- src/shared/UserDTO.java
+++ src/shared/UserDTO.java
@@ -4,6 +4,7 @@ public class UserDTO {
 	
 	//Users email also used as username in the system. 
 	String email = "";
+
 	//Password for the given user
 	String passwd = "";
 	//value of the activated stat
@@ -12,6 +13,11 @@ public class UserDTO {
 	public UserDTO(){
 		
 	}
+	/**
+	 * This construct creates a user with activated value 1
+	 * @param email also used as username
+	 * @param passwd 
+	 */
 	public UserDTO(String email, String passwd){
 		this.email = email;
 		this.passwd = passwd;
@@ -20,6 +26,27 @@ public class UserDTO {
 	public UserDTO(String email, String passwd, int activated){
 		this.email = email;
 		this.passwd = passwd;
+		this.activated = activated;
+	}
+	
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public int getActivated() {
+		return activated;
+	}
+	public void setActivated(int activated) {
 		this.activated = activated;
 	}
 
