e489929a244680651dfacd48bead1c4c28ca9e40
Martin Vieth <martin_123_vieth@hotmail.com>
2015-08-07 16:25:15 +0200

    Added test class for LoginDAO

---
+++ src/test/MySQLLoginDAOTest.java
@@ -0,0 +1,49 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MySQLLoginDAOTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testMySQLLoginDAO() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoginCheck() {
+		fail("Not yet implemented");
+	}
+
+}
