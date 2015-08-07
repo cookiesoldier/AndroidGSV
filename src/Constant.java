64e41f43f61ba461eb4cbf078e25527088834d8c
Signe Geisler <signe.geisler@gmail.com>
2015-08-07 15:40:42 +0200

    Started creating methods for database connection and created methods for user

---
+++ src/Constant.java
@@ -0,0 +1,13 @@
+// erstat konstanterne nedenfor
+
+public abstract class Constant
+{
+	public static final String
+		server					= "localhost",  // database-serveren
+		database				= "AndroidGSV",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "root", // dit brugernavn = dit studienummer 
+		password				= "geisler"; // dit password som du har valgt til din database
+	
+	public static final int
+		port					= 3306;
+}
---
+++ src/MySQLLoginDAO.java
@@ -0,0 +1,57 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MySQLLoginDAO {
+
+	public MySQLLoginDAO() {
+		try {
+			new Connector();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public UserDTO getUser() throws DALException {
+		ResultSet rs = Connector.doQuery("");
+		try {
+			if (!rs.first())
+				throw new DALException("Brugeren findes ikke");
+			return new UserDTO();
+		} catch (SQLException e) {
+			throw new DALException(e.getMessage());
+		}
+	}
+
+	public void createUser(UserDTO user) throws DALException {
+		Connector.doUpdate("");
+	}
+
+	public void updateUser(UserDTO user) throws DALException {
+
+	}
+
+	public List<UserDTO> getUserList() throws DALException {
+		List<UserDTO> list = new ArrayList<UserDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM operatoer");
+		try {
+			while (rs.next()) {
+				list.add(new UserDTO());
+			}
+		} catch (SQLException e) {
+			throw new DALException(e.getMessage());
+		}
+		return list;
+	}
+
+	public void deleteUser(UserDTO user) throws DALException{
+		Connector.doUpdate("");
+	}
+}
